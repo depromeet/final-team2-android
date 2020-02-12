37e7c3a7d40e8f8b10e32d83bfcb417b594fe374
kyung <leeseok8347@gmail.com>
2020-02-12 19:36:08 +0900

    add ChatRoomActivity

---
+++ app/src/main/java/com/def/team2/screen/chatroom/ChatRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.def.team2.screen.chatroom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChatRoomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_room)
+    }
+}
